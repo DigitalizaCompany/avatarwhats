c5e731d0f507f1f0261546ab7630d225ec02ce79
DigitalizaCompany <153322114+DigitalizaCompany@users.noreply.github.com>
2023-12-13 03:03:25 -0300

    Update whatsapp2.js

--- whatsapp2.js
+++ whatsapp2.js
@@ -6,12 +6,6 @@ function criarBarra(var_nome, var_avatar) {
   var userBar = document.createElement("div");
   userBar.className = "user-bar";
 
-  var backButton = document.createElement("div");
-  backButton.className = "back";
-  backButton.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="bi bi-arrow-left-short" viewBox="0 0 16 16">
-  <path fill-rule="evenodd" d="M12 8a.5.5 0 0 1-.5.5H5.707l2.147 2.146a.5.5 0 0 1-.708.708l-3-3a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L5.707 7.5H11.5a.5.5 0 0 1 .5.5z"/>
-</svg>`;
-
   var avatar = document.createElement("div");
   avatar.className = "avatar";
   avatar.innerHTML = '<img src="'+var_avatar+'">';
@@ -22,28 +16,9 @@ function criarBarra(var_nome, var_avatar) {
   
   name.appendChild(varStatus)
 
-  var moreActions = document.createElement("div");
-  moreActions.className = "actions more";
-  // moreActions.innerHTML = '<i class="zmdi zmdi-more-vert"></i>';
-
-  var attachmentAction = document.createElement("div");
-  attachmentAction.className = "actions cam-action";
-  attachmentAction.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-camera-video-fill" viewBox="0 0 16 16">
-  <path fill-rule="evenodd" d="M0 5a2 2 0 0 1 2-2h7.5a2 2 0 0 1 1.983 1.738l3.11-1.382A1 1 0 0 1 16 4.269v7.462a1 1 0 0 1-1.406.913l-3.111-1.382A2 2 0 0 1 9.5 13H2a2 2 0 0 1-2-2V5z"/>
-</svg>`;
-
-  var phoneAction = document.createElement("div"); // Alterado para um elemento de link <a>
-  phoneAction.className = "actions phone-action";
-  phoneAction.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-telephone-fill" viewBox="0 0 16 16">
-  <path fill-rule="evenodd" d="M1.885.511a1.745 1.745 0 0 1 2.61.163L6.29 2.98c.329.423.445.974.315 1.494l-.547 2.19a.678.678 0 0 0 .178.643l2.457 2.457a.678.678 0 0 0 .644.178l2.189-.547a1.745 1.745 0 0 1 1.494.315l2.306 1.794c.829.645.905 1.87.163 2.611l-1.034 1.034c-.74.74-1.846 1.065-2.877.702a18.634 18.634 0 0 1-7.01-4.42 18.634 18.634 0 0 1-4.42-7.009c-.362-1.03-.037-2.137.703-2.877L1.885.511z"/>
-</svg>`;
-
   userBar.appendChild(backButton);
   userBar.appendChild(avatar);
   userBar.appendChild(name);
-  // userBar.appendChild(moreActions);
-  userBar.appendChild(phoneAction);
-  userBar.appendChild(attachmentAction);
 
   if (elementoPai) {
     elementoPai.prepend(userBar);
